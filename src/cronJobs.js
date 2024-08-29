07d472a85cbad5879eb2016e93a4138960346c1f
Lucky13y1y1 <162469602+Lucky13y1y1@users.noreply.github.com>
2024-08-30 00:04:05 +0530

    Update cronJobs.js

--- src/cronJobs.js
+++ src/cronJobs.js
@@ -1,55 +1,72 @@
 const cron = require('cron');
 const { getBalance, claimDailyReward, claimFarmReward } = require('./api');
+let job, balanceCheckJob, dailyRewardJob, farmRewardJob;
 
 function setupCronJob(token) {
-  const job = new cron.CronJob('0 */10 * * *', async () => {
-    console.log('🔄 Starting farming session every 10 hours...'.yellow);
+  job = new cron.CronJob('0 * * * *', async () => {
+    console.log('Starting farming session every 1min...'.yellow);
     await claimFarmReward(token);
-    console.log('🌾 Farming reward claimed!'.green);
+    console.log('Farming reward claimed!'.green);
   });
   job.start();
-  console.log('⏰ Cron job set up to run every 10 hours.'.green);
+  console.log('Cron job set up to run every 1 min.'.green);
+
+  setTimeout(() => {
+    job.stop();
+  }, 900000); // stop the job after 15 minutes
 }
 
 function setupBalanceCheckJob(token) {
-  const job = new cron.CronJob('* * * * *', async () => {
+  balanceCheckJob = new cron.CronJob('* * * * *', async () => {
     const balance = await getBalance(token);
     console.log(
-      `🌾 Updated farming balance: ${balance.farming.balance} BLUM`.green
+      `Updated farming balance: ${balance.farming.balance} BLUM`.green
     );
   });
-  job.start();
-  console.log('⏰ Balance check job set up to run every minute.'.green);
+  balanceCheckJob.start();
+  console.log('Balance check job set up to run every minute.'.green);
+
+  setTimeout(() => {
+    balanceCheckJob.stop();
+  }, 900000); // stop the job after 15 minutes
 }
 
 function setupFarmRewardCron(token) {
-  const job = new cron.CronJob('0 */9 * * *', async () => {
-    console.log('⏰ Running farm reward cron job...'.yellow);
+  farmRewardJob = new cron.CronJob('0 * * * *', async () => {
+    console.log('Running farm reward cron job...'.yellow);
     const reward = await claimFarmReward(token);
 
     if (reward) {
-      console.log('✅ Farm reward claimed successfully!'.green);
+      console.log('Farm reward claimed successfully!'.green);
     }
   });
-  job.start();
+  farmRewardJob.start();
 
-  console.log('🕒 Daily reward cron job scheduled to run every 9 hours.'.green);
+  console.log('Daily reward cron job scheduled to run every 1min.'.green);
+
+  setTimeout(() => {
+    farmRewardJob.stop();
+  }, 900000); // stop the job after 15 minutes
 }
 
 function setupDailyRewardCron(token) {
-  const job = new cron.CronJob('0 0 * * *', async () => {
-    console.log('⏰ Running daily reward cron job...'.yellow);
+  dailyRewardJob = new cron.CronJob('0 0 * * *', async () => {
+    console.log('Running daily reward cron job...'.yellow);
     const reward = await claimDailyReward(token);
 
     if (reward) {
-      console.log('✅ Daily reward claimed successfully!'.green);
+      console.log('Daily reward claimed successfully!'.green);
     }
   });
-  job.start();
+  dailyRewardJob.start();
 
   console.log(
-    '🕒 Daily reward cron job scheduled to run every 24 hours.'.green
+    'Daily reward cron job scheduled to run every 24 hours.'.green
   );
+
+  setTimeout(() => {
+    dailyRewardJob.stop();
+  }, 900000); // stop the job after 15 minutes
 }
 
 module.exports = {
